5e3bd6ef9131eca7967c5df2169f128a225a1f7e
laynezhang <meetzhang@hotmail.com>
2024-11-25 01:57:26 +0800

    接入waline

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,15 @@
 import {NextPage} from 'next';
+import Comment from '@/components/Comment';
 
 const About: NextPage = () => {
-    return <div>hello</div>;
+    const serverURL = process.env.WALINE_BACKEND_URL ?? '';
+
+    return (
+        <div>
+            hello
+            <Comment serverURL={serverURL} />
+        </div>
+    );
 };
 
 export default About;
--- src/app/themeProvider.tsx
+++ src/app/themeProvider.tsx
@@ -3,7 +3,7 @@ import {ThemeProvider as NextThemeProvider} from 'next-themes';
 
 const ThemeProvider: React.FC<PropsWithChildren> = ({children}) => {
     return (
-        <NextThemeProvider attribute="class" enableSystem themes={['light', 'dark']}>
+        <NextThemeProvider attribute={['class', 'data-theme']} enableSystem>
             {children}
         </NextThemeProvider>
     );
