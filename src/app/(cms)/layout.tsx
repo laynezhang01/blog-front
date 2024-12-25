48ae5b182bbf2cf3144e9bababfb44c43d1a1ab3
laynezhang <meetzhang@hotmail.com>
2024-12-25 23:24:47 +0800

    remove sanity

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -1,7 +1,11 @@
-export default function RootLayout({children}) {
+import React, {PropsWithChildren} from 'react';
+
+const RootLayout: React.FC<PropsWithChildren> = ({children}) => {
     return (
         <html lang="en">
             <body>{children}</body>
         </html>
     );
 }
+
+export default RootLayout;
