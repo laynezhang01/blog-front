0a346ba5ee01793faeec80d95790eecee9b56cce
laynezhang <meetzhang@hotmail.com>
2024-12-25 23:25:14 +0800

    remove sanity

--- src/app/api/outstatic/[[...ost]]/route.ts
+++ src/app/api/outstatic/[[...ost]]/route.ts
@@ -1,8 +1,4 @@
 import {OutstaticApi} from 'outstatic';
 
-export const GET = opt => {
-    console.log(opt);
-};
-
-// export const GET = OutstaticApi.GET
+export const GET = OutstaticApi.GET;
 export const POST = OutstaticApi.POST;
