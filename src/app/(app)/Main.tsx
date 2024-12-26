72cc3db2387722bea5e990260b8c7801881c30ae
Layne <52080234+laynezhang01@users.noreply.github.com>
2024-12-26 23:15:53 +0800

    Update Main.tsx

--- src/app/(app)/Main.tsx
+++ src/app/(app)/Main.tsx
@@ -2,15 +2,8 @@ import React from 'react';
 import Banner from '@/components/Banner';
 import PostsList from '@/components/Post/List';
 import {getAllPosts} from '@/libs/post';
-import {load} from 'outstatic/server';
 
 async function Main() {
-    const db = await load();
-    const allPosts = await db
-        .find({collection: 'posts'}, ['title', 'publishedAt', 'slug', 'coverImage', 'description', 'tags'])
-        .sort({publishedAt: -1})
-        .toArray();
-    console.log(allPosts);
     const posts = await getAllPosts();
     return (
         <div className="flex flex-col gap-8">
