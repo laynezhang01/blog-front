e9e010ce946c82a4f91436e4d025cc9ea0435433
laynezhang <meetzhang@hotmail.com>
2024-12-26 22:31:30 +0800

    cms

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -2,7 +2,7 @@ import React, {PropsWithChildren} from 'react';
 
 const RootLayout: React.FC<PropsWithChildren> = ({children}) => {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body>{children}</body>
         </html>
     );
