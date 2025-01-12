750adc1e8d3ede1c8f70c14c76c010c7778ce952
laynezhang <meetzhang@hotmail.com>
2025-01-13 02:33:32 +0800

    header修改

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -64,5 +64,5 @@ export default {
             }
         }
     },
-    plugins: [require('tailwindcss-animate'), require('@tailwindcss/line-clamp')]
+    plugins: [require('tailwindcss-animate')]
 };
